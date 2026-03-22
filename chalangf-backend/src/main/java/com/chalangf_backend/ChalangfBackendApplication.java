469fd462c67199b0b506519869b54c169c460bcc
Yuvraj <kabadwalyuvraj@gmail.com>
2026-03-22 19:35:07 +0530

    Phase 2

---
+++ chalangf-backend/src/main/java/com/chalangf_backend/ChalangfBackendApplication.java
@@ -0,0 +1,13 @@
+package com.chalangf_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChalangfBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChalangfBackendApplication.class, args);
+	}
+
+}
